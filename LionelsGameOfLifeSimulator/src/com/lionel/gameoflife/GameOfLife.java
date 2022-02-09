6112567d4b58bc0b7a51c71e07a04252f687682c
ltarazona <ltarazona@s09-prof>
2022-02-09 16:42:06 +0100

    Añadida clase con main (esta vez sí)

---
+++ LionelsGameOfLifeSimulator/src/com/lionel/gameoflife/GameOfLife.java
@@ -0,0 +1,9 @@
+package com.lionel.gameoflife;
+
+public class GameOfLife {
+	public static void main(String[] args) {
+		GameOfLifeWindow game = new GameOfLifeWindow();
+		game.run();
+		System.exit(0);
+	}
+}
